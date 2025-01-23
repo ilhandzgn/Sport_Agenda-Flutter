f5cf2b7f114fd3d2b5b459cd1d0e9a54ddf09eef
İlhan Düzgün <ilhandzgn.58@gmail.com>
2025-01-23 16:29:35 +0300

    main dart güncelledim
    gece gündüz modu için buton ekledim

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,59 @@
 import 'package:flutter/material.dart';
 import 'core/routes.dart';
-// ignore: unused_import
 import 'core/themes.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized(); // Flutter widget'larını başlat
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Başlangıç teması: Gündüz modu
+  ThemeMode _themeMode = ThemeMode.light;
+
+  // Temayı değiştiren fonksiyon
+  void toggleTheme() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter App',
-      themeMode: ThemeMode.light,
+      title: 'SPORT AGENDA',
+      themeMode: _themeMode,
       theme: lightTheme,
       darkTheme: darkTheme,
-      routerConfig: router, // go_router yapılandırmamızı kullan
-      debugShowCheckedModeBanner: false, // Debug bandını kaldır
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+      // Bir düğme ekleyerek temayı değiştirme
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('SPORT AGENDA'),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  _themeMode == ThemeMode.light
+                      ? Icons.dark_mode
+                      : Icons.light_mode,
+                ),
+                onPressed: toggleTheme,
+              ),
+            ],
+          ),
+          body: child,
+        );
+      },
     );
   }
 }
